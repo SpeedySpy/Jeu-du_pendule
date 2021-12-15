72a10a64398d6553fa4b76824d898327daf3f0d9
Biraveen SIVAHARAN <75846636+SpeedySpy@users.noreply.github.com>
2021-12-15 12:15:58 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, {Component} from 'react'
+import './App.css';
+
+
+
+function App (props){
+  <h1>Bienvenu sur notre pendule en ligne ! Pret a gagner ?</h1>
+
+  return (
+    <div>
+      <mot name = "papier"/>,
+      <mot name = "papier"/>,
+      
+
+
+
+
+    </div>
+   
+
+
+
+
+
+  );
+
+
+
+
+
+  /* state = {
+    mots :["papier","javascript","programmeur","criniere","voiture","course"],
+    currentWord:null,
+    alphabet:"A B C D E F G H I J K L M N O P Q R S T, U, V, W, X, Y, Z."
+  }
+
+
+
+  initGame =()=> {
+    this.setState({currentWord:"javascript"});
+
+
+
+
+
+
+  }
+
+   render(){
+    return (
+      <div id="game">
+        <h1><center>Bienvenu sur le jeu de pendule </center></h1>
+        <div>{this.state.currentWord}</div>
+        <div>a b c d e f</div>
+
+      </div>
+
+
+
+    )
+
+
+   }
+
+
+
+*/
+
+
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+export default App;
